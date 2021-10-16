d18ebc2b392bae752e1b8d63f629a09911b532b4
veritas919 <olackner@smu.edu>
2021-10-16 13:05:25 -0500

    Navbar with routing added

--- frontend/src/components/NavigationBar/NavigationBar.js
+++ frontend/src/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,27 @@
 import React from "react"
-
+import { Redirect } from "react-router-dom"
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-function NavigationBar(props){
-  return (
+class NavigationBar extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      redirect: null
+    }
+
+    this.handlePageChange = this.handlePageChange.bind(this); 
+  }
+  
+  handlePageChange(pageName){
+    this.setState({redirect: pageName})
+  }
+  
+  render(){
+    return (
+    
+    this.state.redirect ? <Redirect to = { this.state.redirect} />  :
     <Stack
               sx={{ mt: 4, mb: 2 }}
               direction="row"
@@ -12,11 +29,13 @@ function NavigationBar(props){
               justifyContent="center"
             >
             
-              <Button variant = {props.page === "Movies" ? "contained" : "outlined"}>Movies</Button>
-              <Button variant = {props.page === "Books" ? "contained" : "outlined"} >Books</Button>
-              <Button variant = {props.page === "Friends" ? "contained" : "outlined"} >Friends</Button>
+              <Button variant = {this.props.page === "Movies" ? "contained" : "outlined"} onClick = {() => this.handlePageChange("/movies")}>Movies</Button>
+              <Button variant = {this.props.page === "Books" ? "contained" : "outlined"} onClick = {() => this.handlePageChange("/books")} >Books</Button>
+              <Button variant = {this.props.page === "Friends" ? "contained" : "outlined"} onClick = {() => this.handlePageChange("/friends")} >Friends</Button>
     </Stack>
   )
+
+  }
 }
 
 export default NavigationBar; 
