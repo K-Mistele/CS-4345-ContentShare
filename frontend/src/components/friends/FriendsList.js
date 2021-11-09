8ecdd71094ce76328efc4a1c79683c0ce8e88cd8
YongjiaXu <yongjiax@smu.edu>
2021-11-08 21:25:58 -0600

    refractor movie UI

--- frontend/src/components/friends/FriendsList.js
+++ frontend/src/components/friends/FriendsList.js
@@ -7,67 +7,88 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import NavigationBar from "../NavigationBar/NavigationBar"
 import Header from "../Header/Header"
-
+import Container from '@mui/material/Container';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
 import friendData from "../../temp_data/friendData"
 
-class FriendsList extends React.Component{
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
-    constructor(){
+class FriendsList extends React.Component {
+
+    constructor() {
         super();
         this.state = {
-            friends: []
+            friends: [],
+            openAddFriendDialog: false
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setState({
             friends: friendData
         })
     }
 
-    render(){
+    render() {
 
         return (
-            <div className = "changeLATER">
-            <Header/> 
-            <NavigationBar page = "Friends"/> 
-            <Typography sx={{ fontSize: 20, mt:4 }} color="text.primary" gutterBottom variant = "h1">
-                        Friends
-                    </Typography>
+            <div className="changeLATER">
+                <Header />
+                <NavigationBar page="Friends" />
+                <Container maxWidth="md" sx= {{mt:5}}>
+                <Stack
+                sx={{ pt: 2 }}
+                direction="row"
+                spacing={2}
+                justifyContent="right"
+            >
+                <Button onClick={() => { this.setState({ openAddFriendDialog: true }) }} color="primary" startIcon={<AddCircleOutlineIcon />}
+                variant="text">Add a New Friend</Button>
+                <Button onClick={() => { this.setState({ openAddFriendDialog: true }) }} color="primary" startIcon={<CircleNotificationsIcon />}
+                variant="text">Friend Invites</Button>
+            </Stack>
+                <Typography sx={{ fontSize: 20, mt: 4 }} color="text.primary" gutterBottom variant="h1">
+                    Friends
+                </Typography>
 
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 450 }} aria-label="simple table">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell sx = {{ fontWeight: 600 }}>First Name</TableCell>
-                        <TableCell sx = {{ fontWeight: 600 }} align="right">Last Name </TableCell>
-                        <TableCell sx = {{ fontWeight: 600 }} align="right">Handle&nbsp;</TableCell>
-                    </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {this.state.friends.map((friend) => (
-                        <TableRow
-                        key={friend.id}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                        <TableCell component="th" scope="row">
-                            {friend.firstName}
-                        </TableCell>
-                        <TableCell align="right">{friend.lastName}</TableCell>
-                        <TableCell align="right">{friend.handle}</TableCell>
-                        </TableRow>
-                    ))}
-                    </TableBody>
-                </Table>
-    </TableContainer>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 450 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ fontWeight: 600 }}>First Name</TableCell>
+                                <TableCell sx={{ fontWeight: 600 }} align="right">Last Name </TableCell>
+                                <TableCell sx={{ fontWeight: 600 }} align="right">Handle&nbsp;</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.state.friends.map((friend) => (
+                                <TableRow
+                                    key={friend.id}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {friend.firstName}
+                                    </TableCell>
+                                    <TableCell align="right">{friend.lastName}</TableCell>
+                                    <TableCell align="right">
+                                        <Button href='/'>{friend.handle}</Button>
+                                        </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                </Container>
             </div>
         )
     }
 }
 
-export default FriendsList; 
+export default FriendsList;
--- frontend/src/temp_data/movieReviewsData.js
+++ frontend/src/temp_data/movieReviewsData.js
@@ -1,21 +1,27 @@
 const movieReviews = [
     {
         id: 1,
+        'movieTitle': 'movie Title1',
         'reviewTitle': "Jaws",
         'reviewText': "Not scary at all. Attack scenes were WAY too fake for me",
-        'rating': 2
+        'reviewRating': 2,
+        'reviewImgUrl': 'https://m.media-amazon.com/images/I/51aUE4WyciL._AC_SY679_.jpg'
     },
     {
         id: 2,
+        'movieTitle': 'movie Title2',
         'reviewTitle': "The Shining",
         'reviewText': "SO scary I couldn't sleep for weeks",
-        'rating': 5       
+        'reviewRating': 5,
+        'reviewImgUrl': 'https://m.media-amazon.com/images/I/51f4xOLpVTL._AC_.jpg'   
     },
     {
         id: 3,
+        'movieTitle': 'movie Title3',
         'reviewTitle': "School of Rock",
-        'reviewText': "Jack Black is dgsadsahgsdfh sdgfsdfhgwrdsf hwrygtrsf hyywrsht w r thwtrfhterdhetrfjrgjksdaorti ygqeriltuof5uiaq g an amazing actor!!! Honestly, WOW. I felt like I laughed the whole time I was watching this.",
-        'rating': 5       
+        'reviewText': "Jack Black is dgsadsahgsdfh sdgfsdfhgwrdsf hwrygtsadsahgsdfh sdgfsdfhgwrdsf hwrygtsadsahgsdfh sdgfsdfhgwrdsf hwrygtsadsahgsdfh sdgfsdfhgwrdsf hwrygtsadsahgsdfh sdgfsdfhgwrdsf hwrygtsadsahgsdfh sdgfsdfhgwrdsf hwrygtsadsahgsdfh sdgfsdfhgwrdsf hwrygtrsf hyywrsht w r thwtrfhterdhetrfjrgjksdaorti ygqeriltuof5uiaq g an amazing actor!!! Honestly, WOW. I felt like I laughed the whole time I was watching this.",
+        'reviewRating': 5,
+        'reviewImgUrl': 'https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/school-of-rock.jpg?itok=f756FIKx'       
     }
     
 ]
